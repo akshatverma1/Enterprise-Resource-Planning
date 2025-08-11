20ab6aa2d63b856ccd2969cb8d8a0fcb2d689f0f
akshatverma1 <akshaverma14@gmail.com>
2025-08-11 22:21:05 +0530

    b-8

--- src/Components/mockProducts_automobile.js
+++ src/Components/mockProducts_automobile.js
@@ -1,5 +1,5 @@
 export async function getAutomobileData() {
-    const response = await fetch("http://localhost:4000/api/products");
+    const response = await fetch("https://enterprise-resource-planning-backen.vercel.app/api/products");
     console.log(response);
     const jsonData = await response.json();
     return jsonData;
